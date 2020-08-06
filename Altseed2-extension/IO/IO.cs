969dd62c68fe8ad9fb2416840228c2f6491bb9e1
GON <hutabasuzukiryu@live.jp>
2020-08-07 03:29:21 +0900

    add: IO

---
+++ Altseed2-extension/IO/IO.cs
@@ -0,0 +1,33 @@
+﻿using Altseed2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altseed2Extension.IO
+{
+    /// <summary>
+    /// ファイル入出力系
+    /// </summary>
+    public class IO
+    {
+        public static MemoryStream GetStream(string path)
+        {
+            if (path == null) throw new ArgumentNullException("path is null");
+            var temp = StaticFile.Create(path);
+            if (temp == null) throw new FileNotFoundException(path + "が見つかりません");
+            var result = temp.Buffer;
+            return new MemoryStream(result);
+        }
+
+        public static async Task<MemoryStream> GetStreamAsync(string path)
+        {
+            if (path == null) throw new ArgumentNullException("path is null");
+            var temp = await StaticFile.CreateAsync(path);
+            if (temp == null) throw new FileNotFoundException(path + "が見つかりません");
+            var result = temp.Buffer;
+            return new MemoryStream(result);
+        }
+    }
+}
