d5ed43d1a302e0a20451bb5b5c0a9fe2d310362d
GON <hutabasuzukiryu@live.jp>
2020-08-07 00:10:32 +0900

    add: Texture2DExtension

---
+++ Altseed2-extension/Extension/Texture2DExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Altseed2;
+
+namespace Altseed2Extension.Extension
+{
+    public static class Texture2DExtension
+    {
+        public static string ErrorImage { get; set; } = "Static/error.png";
+
+        /// <summary>
+        /// テクスチャをロードする
+        /// </summary>
+        /// <param name="path">パス</param>
+        /// <returns>テクスチャ</returns>
+        public static Texture2D Load(string path)
+        {
+            var texture = Texture2D.Load(path);
+            if (texture == null)
+            {
+                texture = Texture2D.Load(ErrorImage);
+                Engine.Log.Error(LogCategory.User, $"'{path}' is not found.");
+            }
+            return texture;
+        }
+
+        /// <summary>
+        /// テクスチャを非同期ロードする
+        /// </summary>
+        /// <param name="path">パス</param>
+        /// <returns>テクスチャ</returns>
+        public static async Task<Texture2D> LoadAsync(string path)
+        {
+            var texture = await Texture2D.LoadAsync(path);
+            if (texture == null)
+            {
+                texture = await Texture2D.LoadAsync(ErrorImage);
+                Engine.Log.Error(LogCategory.User, $"'{path}' is not found.");
+            }
+            return texture;
+        }
+    }
+}
---
+++ Test/Extension.cs
@@ -0,0 +1,37 @@
+﻿using Altseed2;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Altseed2Extension.Extension;
+
+namespace Test
+{
+    [TestFixture]
+    public class Extension
+    {
+        [Test, Apartment(ApartmentState.STA)]
+        public void Texture2DExtension()
+        {
+            Engine.Initialize("Texture2DExtension", 800, 600, new Configuration());
+            Altseed2Extension.Extension.Texture2DExtension.ErrorImage = "../TestData/IO/AltseedPink.jpeg";
+
+            var sprite = new SpriteNode();
+            sprite.Position = new Vector2F(400, 400);
+            sprite.Texture = Altseed2Extension.Extension.Texture2DExtension.Load("../TestData/IO/hoge.png");
+            sprite.Texture = Altseed2Extension.Extension.Texture2DExtension.Load("../TestData/IO/AltseedPink.png");
+            sprite.CenterPosition = (sprite.Texture?.Size.To2F() ?? default) / 2.0f;
+            Engine.AddNode(sprite);
+
+            int count = 0;
+            while (Engine.DoEvents())
+            {
+                if (count++ > 200) break;
+                Engine.Update();
+            }
+
+            Engine.Terminate();
+        }
+    }
+}
