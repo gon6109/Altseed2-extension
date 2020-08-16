e84b7579608a9619fc550d1350c4d7b78054923f
GON <hutabasuzukiryu@live.jp>
2020-08-17 05:28:34 +0900

    update: Editor

--- Altseed2-extension/Editor/Editor.cs
+++ Altseed2-extension/Editor/Editor.cs
@@ -10,11 +10,14 @@ namespace Altseed2Extension.Editor
     {
         private static object selected;
         private static RenderTexture main;
-        private static CameraNode mainCamera;
 
         private static IEnumerable<Tool.ToolElement> selectedToolElements;
         public static List<TextureBase> TextureBases = new List<TextureBase>();
         public static List<Font> Fonts = new List<Font>();
+
+        public static Vector2F MousePosition { get; private set; }
+
+        private static CameraNode MainCamera { get; set; }
 
         internal static Tool.TextureBaseToolElement TextureBrowserTarget { get; set; }
         internal static Tool.FontToolElement FontBrowserTarget { get; set; }
@@ -45,12 +48,12 @@ namespace Altseed2Extension.Editor
 
             main = RenderTexture.Create(Engine.WindowSize - new Vector2I(600, 18), TextureFormat.R8G8B8A8_UNORM);
 
-            mainCamera = new CameraNode();
-            mainCamera.IsColorCleared = true;
-            mainCamera.ClearColor = new Color(50, 50, 50);
-            mainCamera.Group = 63;
-            mainCamera.TargetTexture = main;
-            Engine.AddNode(mainCamera);
+            MainCamera = new CameraNode();
+            MainCamera.IsColorCleared = true;
+            MainCamera.ClearColor = new Color(50, 50, 50);
+            MainCamera.Group = 63;
+            MainCamera.TargetTexture = main;
+            Engine.AddNode(MainCamera);
 
             first = true;
 
@@ -107,7 +110,7 @@ namespace Altseed2Extension.Editor
             if (windowSize != Engine.WindowSize)
             {
                 main = RenderTexture.Create(Engine.WindowSize - new Vector2I(600, (int)menuHeight), TextureFormat.R8G8B8A8_UNORM);
-                mainCamera.TargetTexture = main;
+                MainCamera.TargetTexture = main;
                 windowSize = Engine.WindowSize;
             }
 
@@ -119,11 +122,15 @@ namespace Altseed2Extension.Editor
                 | ToolWindowFlags.NoResize | ToolWindowFlags.NoScrollbar
                 | ToolWindowFlags.NoScrollbar | ToolWindowFlags.NoTitleBar | ToolWindowFlags.NoScrollWithMouse;
 
+            Engine.Tool.PushStyleVarVector2F(ToolStyleVar.WindowPadding, default);
+            Engine.Tool.PushStyleVarFloat(ToolStyleVar.WindowRounding, 0);
             if (Engine.Tool.Begin("Main", flags))
             {
+                MousePosition = Engine.Mouse.Position - Engine.Tool.GetWindowPos();
                 Engine.Tool.Image(main, main.Size, default, new Vector2F(1, 1), new Color(255, 255, 255), new Color());
                 Engine.Tool.End();
             }
+            Engine.Tool.PopStyleVar(2);
         }
 
         private static void UpdateNodeTreeWindow()
@@ -132,7 +139,7 @@ namespace Altseed2Extension.Editor
             {
                 foreach (var node in nodes)
                 {
-                    if (node == mainCamera) continue;
+                    if (node == MainCamera) continue;
 
                     Engine.Tool.PushID(node.GetHashCode());
                     var flags = ToolTreeNodeFlags.OpenOnArrow;
