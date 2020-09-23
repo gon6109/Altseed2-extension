f1e750fe67370dfbbfbb5ed3562acb5f64d8f33b
GON <hutabasuzukiryu@live.jp>
2020-09-23 19:34:19 +0900

    add: Coroutine

---
+++ Altseed2-extension/Extension/Coroutine.cs
@@ -0,0 +1,44 @@
+﻿using Altseed2;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altseed2Extension.Extension
+{
+    public static class Coroutine
+    {
+        public static IEnumerator<IEnumerator> PlayCoroutine(IEnumerator<IEnumerator> coroutine)
+        {
+            var currentCoroutine = coroutine;
+            Stack<IEnumerator<IEnumerator>> stackCroutine = new Stack<IEnumerator<IEnumerator>>();
+            while (true)
+            {
+                if (currentCoroutine?.MoveNext() ?? false)
+                {
+                    if (currentCoroutine?.Current is IEnumerator<IEnumerator> sub)
+                    {
+                        stackCroutine.Push(currentCoroutine);
+                        currentCoroutine = sub;
+                    }
+                }
+                else
+                {
+                    if (stackCroutine.Count == 0) yield break;
+                    currentCoroutine = stackCroutine.Pop();
+                }
+                yield return null;
+            }
+        }
+
+        public static IEnumerator<IEnumerator> Delay(float duration)
+        {
+            float time = duration;
+            while (time > 0)
+            {
+                time -= Engine.DeltaSecond;
+                yield return null;
+            }
+        }
+    }
+}
